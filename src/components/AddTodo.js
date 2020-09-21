78fae88a6ef5da36df4af8d7f892e5714e87382d
DJ Viking <anders.swedishviking@gmail.com>
2020-09-20 20:02:27 -0400

    updating the PropTypes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
 
@@ -43,6 +44,10 @@ export class AddTodo extends Component {
       </form>
     )
   }
+}
+
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
 }
 
 export default AddTodo;
