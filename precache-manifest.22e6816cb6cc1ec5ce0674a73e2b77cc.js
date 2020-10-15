2e69589a28c037bc0f89d1b85c65092ff778507d
DJ Viking <anders.swedishviking@gmail.com>
2020-10-15 01:04:14 -0400

    Updates

---
+++ precache-manifest.22e6816cb6cc1ec5ce0674a73e2b77cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d450ad15bb1191931602a4ee2a4feeb2",
+    "url": "/react-todo-video-follow/index.html"
+  },
+  {
+    "revision": "5447060036758cd3efb9",
+    "url": "/react-todo-video-follow/static/css/main.0dcc4e88.chunk.css"
+  },
+  {
+    "revision": "5acafdc6f963879a7dd6",
+    "url": "/react-todo-video-follow/static/js/2.8e769596.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-video-follow/static/js/2.8e769596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5447060036758cd3efb9",
+    "url": "/react-todo-video-follow/static/js/main.9fc0e99c.chunk.js"
+  },
+  {
+    "revision": "9f5372d4a0dc4f539d54",
+    "url": "/react-todo-video-follow/static/js/runtime-main.1c95476e.js"
+  }
+]);
